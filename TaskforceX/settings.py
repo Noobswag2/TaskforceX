b84ff26e897dfe4355b8a2b2153e09ba91d5875a
Noobswag2 <kpate237@charlotte.edu>
2025-02-15 21:18:35 -0500

    Static Files

--- TaskforceX/settings.py
+++ TaskforceX/settings.py
@@ -113,6 +113,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -120,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

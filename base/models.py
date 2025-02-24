a09cd53e13492f84d01f5dedc52c33da46521c6f
Noobswag2 <kpate237@charlotte.edu>
2025-02-23 19:36:08 -0500

    Integrating Custom User Model

--- base/models.py
+++ base/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True, )
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
@@ -13,7 +22,7 @@ class Topic(models.Model):
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True) 
     name = models.CharField(max_length=200)
     description = models.TextField(null=True)
     participants = models.ManyToManyField(
@@ -38,5 +47,5 @@ class Message(models.Model):
     class Meta:
         ordering = ['-updated', '-created']  
     
-    def _str_(self):
-        return self.body[0:50]
+    def __str__(self):
+         return self.body[0:50]
